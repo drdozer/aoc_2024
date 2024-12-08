e672a2767fce873b0450c93d6dc592875f5484eb
Matthew Pocock <turingatemyhamster@gmail.com>
2024-12-08 12:46:55 +0000

    Added stub for day 8

---
+++ src/day8.rs
@@ -0,0 +1,22 @@
+use aoc_runner_derive::aoc;
+
+#[aoc(day8, part1)]
+pub fn part1(input: &str) -> u64 {
+    todo!()
+}
+
+#[aoc(day8, part2)]
+pub fn part2(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {}
+
+    #[test]
+    fn test_part2() {}
+}
