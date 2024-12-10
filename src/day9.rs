af115e61fc6fd90a58231617d2d6adfd6b6c4d88
Matthew Pocock <turingatemyhamster@gmail.com>
2024-12-10 00:33:10 +0000

    I am not confident, but it seems to perhaps work for part 1.

--- src/day9.rs
+++ src/day9.rs
@@ -30,15 +30,6 @@ pub struct FileDetails {
 pub fn get_file_details(input: &[u8], id: u64) -> FileDetails {
     unsafe {
         let p = id as usize * 2;
-
-        // println!("p: {}. id: {}", p, id);
-        // println!(
-        //     "{}=`{}`, {}=`{}`",
-        //     input[p],
-        //     input[p] as char,
-        //     input[p + 1],
-        //     input[p + 1] as char
-        // );
         let used = input.get_unchecked(p).unchecked_sub(b'0') as u64;
         let free = input.get_unchecked(p + 1).wrapping_sub(b'0') as u64;
 
@@ -68,7 +59,7 @@ fn part1(input: &str) -> u64 {
     let total_blocks: u64 = (0..file_count)
         .map(|i| get_file_details(input, i as u64).used)
         .sum();
-    println!("total_blocks: {}", total_blocks);
+    // println!("total_blocks: {}", total_blocks);
 
     let mut checksum = 0;
 
@@ -83,10 +74,10 @@ fn part1(input: &str) -> u64 {
         let mut left = get_file_details(input, left_id);
 
         // Calculate the sum of the blocks occupied by this.
-        let left_checksum = sum_checksum_range_loop(block, left.used, left_id as u64);
-        println!(
-            "block: {block}\tleft: {left_id}\t{left:?}\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tAdvancing\t{}x{left_id} ({block}..{})= {left_checksum}",left.used, block + left.used
-        );
+        let left_checksum = sum_checksum_range(block, left.used, left_id as u64);
+        // println!(
+        //     "block: {block}\tleft: {left_id}\t{left:?}\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tAdvancing\t{}x{left_id} ({block}..{})= {left_checksum}",left.used, block + left.used
+        // );
         checksum += left_checksum;
 
         // increment left and block to point to the next empty space
@@ -104,9 +95,9 @@ fn part1(input: &str) -> u64 {
         while left.free > 0 && right_id > left_id {
             // If there's nothing remaining in the right file, we pull from the end.
             if right_remaining == 0 {
-                println!(
-                    "block: {block}\tleft: {left_id}\t{left:?}\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tPulling from the end"
-                );
+                // println!(
+                //     "block: {block}\tleft: {left_id}\t{left:?}\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tPulling from the end"
+                // );
                 right_id -= 1;
                 let right = get_file_details(input, right_id);
                 right_remaining = right.used;
@@ -114,10 +105,10 @@ fn part1(input: &str) -> u64 {
 
             // We can pull can_fill values from the remaining right file.
             let can_fill = left.free.min(right_remaining);
-            let right_fragment_checksum = sum_checksum_range_loop(block, can_fill, right_id);
-            println!(
-                "block: {block}\tleft: {left_id}\t{left:?}\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tfilling\t{can_fill}x{right_id} ({block}..{}) = {right_fragment_checksum}", block+can_fill
-            );
+            let right_fragment_checksum = sum_checksum_range(block, can_fill, right_id);
+            // println!(
+            //     "block: {block}\tleft: {left_id}\t{left:?}\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tfilling\t{can_fill}x{right_id} ({block}..{}) = {right_fragment_checksum}", block+can_fill
+            // );
             checksum += right_fragment_checksum;
             right_remaining -= can_fill;
             left.free -= can_fill;
@@ -126,19 +117,19 @@ fn part1(input: &str) -> u64 {
     }
 
     // It is possible that the last right file was not fully consumed.
-    println!(
-        "Last right file: {} with {} remaining",
-        right_id, right_remaining
-    );
+    // println!(
+    //     "Last right file: {} with {} remaining",
+    //     right_id, right_remaining
+    // );
 
-    let last_remaining_checksum = sum_checksum_range_loop(block, right_remaining, right_id);
-    println!(
-            "block: {block}\tleft: {left_id}\t\t\t\t\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tEnd\t{right_remaining}x{right_id} ({block}..{}) = {last_remaining_checksum}", block + right_remaining
-        );
+    let last_remaining_checksum = sum_checksum_range(block, right_remaining, right_id);
+    // println!(
+    //         "block: {block}\tleft: {left_id}\t\t\t\t\tright: {right_id}\tbackfill: {right_remaining}\tchecksum: {checksum}\tEnd\t{right_remaining}x{right_id} ({block}..{}) = {last_remaining_checksum}", block + right_remaining
+    //     );
     checksum += last_remaining_checksum;
     block += right_remaining;
 
-    assert_eq!(
+    debug_assert_eq!(
         block, total_blocks,
         "The last block does not match the total blocks."
     );
@@ -166,7 +157,15 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 1);
+        assert_eq!(part1(INPUT), 6332189866718);
+    }
+
+    #[test]
+    fn test_part1_alt() {
+        assert_eq!(
+            part1(include_str!("../input/2024/day9_backup.txt")),
+            6386640365805
+        );
     }
 
     #[test]
@@ -176,7 +175,7 @@ mod tests {
 
     #[test]
     fn test_sum_range() {
-        for i in 1..100 {
+        for i in 0..100 {
             for j in 1..100 {
                 assert_eq!(sum_range(i, j), (i..i + j).sum(), "Failed for {}, {}", i, j);
             }
@@ -185,9 +184,9 @@ mod tests {
 
     #[test]
     fn test_sum_checksum_range() {
-        for i in 1..100 {
+        for i in 0..100 {
             for j in 1..10 {
-                for k in 1..100 {
+                for k in 0..100 {
                     assert_eq!(
                         sum_checksum_range(i, j, k),
                         sum_checksum_range_loop(i, j, k),
@@ -203,8 +202,10 @@ mod tests {
 
     #[test]
     fn test_sum_checksum_range_values() {
+        // A particular checksum example
         let s = (49989..(49989 + 8)).sum::<u64>() * 5218;
         assert_eq!(sum_checksum_range(49989, 8, 5218), s);
         assert_eq!(sum_checksum_range_loop(49989, 8, 5218), s);
+        assert_eq!(s, 2086886920);
     }
 }
